cc1896f0bc7e383391aa24430d75aa9335c4caef
Justinas Marcinka <jumar@dbml.dk>
2017-09-12 17:19:24 +0300

    Implement sum function

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-
+function sum(arg1, arg2) {
+	return arg1 + arg2
+}
 function main() {
-	const arg1 = process.argv[2]
-	const arg2 = process.argv[4]
+	const arg1 = parseInt(process.argv[2])
+	const arg2 = parseInt(process.argv[4])
 	const operation = process.argv[3]
-	console.log(arg1, operation, arg2)
+
+	switch(operation) {
+		case '+':
+			console.log(sum(arg1, arg2))
+			break;
+		default:
+			console.err('Unknown operation: ' + operation)	
+			process.exit(1);
+	}
 }
 
 main();
