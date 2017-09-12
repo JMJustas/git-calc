ce600e380c36fc17da9ccf01b30bd30e502d8109
Domas Petkevicius <dpet@dbml.dk>
2017-09-12 17:43:36 +0300

    Extract substract to new function

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 function sum(arg1, arg2) {
 	return arg1 + arg2
 }
+function sub(arg1, arg2) {
+    return arg1 - arg2;
+}
+
 function main() {
 	const arg1 = parseInt(process.argv[2])
 	const arg2 = parseInt(process.argv[4])
@@ -11,7 +15,7 @@ function main() {
 			console.log(sum(arg1, arg2))
 			break;
         case '-':
-            console.log((arg1 - arg2));
+            console.log(sub(arg1, arg2));
             break;
 		default:
 			console.err('Unknown operation: ' + operation)	
